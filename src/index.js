50409bd48f117f43ae9a1dcc757079f1af0be8ad
LeslieAine <aineleslie@gmail.com>
2023-02-02 14:24:56 +0300

    Run js files

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import { renderToDoList } from './modules/listTasks.js';
+import toDoTasks from './toDoTasks.js';
+import './modules/eventListeners.js';
+import './styles/style.css';
+
+const toDoList = document.querySelector('.todo-list-ul');
+
+renderToDoList(toDoTasks, toDoList);
